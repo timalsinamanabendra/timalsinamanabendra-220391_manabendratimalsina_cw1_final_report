85cc48761ed770d25a409d3e39eb34ee8b8961da
timalsinamanabendra <97384274+timalsinamanabendra@users.noreply.github.com>
2023-08-09 02:12:42 +0545

    Create deleteproduct.php

---
+++ deleteproduct.php
@@ -0,0 +1,25 @@
+<?php
+@include 'config.php';
+
+if(!isset($_SESSION['seller_name'])){
+   header('location:login.php');
+};
+if (isset($_GET['product_id'])) {
+    $product_id = $_GET['product_id'];
+
+    $query = "DELETE FROM `images` WHERE product_id = '$product_id'";
+    $query_run = mysqli_query($conn, $query);
+
+    if ($query_run) {
+        echo "<script type='text/javascript'>alert('Product deleted');</script>";
+    } else {
+        echo "<script type='text/javascript'>alert('Failed to delete user');</script>";
+    }
+
+    // Redirect back to the userdata.php page after deletion
+    header('Location: myproduct.php');
+}
+?>
+
+
+
