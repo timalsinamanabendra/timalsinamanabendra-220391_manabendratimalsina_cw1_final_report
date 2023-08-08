6091137aa4061891b00ab5ce1aeae4aff55e15ea
timalsinamanabendra <97384274+timalsinamanabendra@users.noreply.github.com>
2023-08-09 02:26:09 +0545

    Create signup view.php

---
+++ signup view.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Ecommerce</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- bootstrap links -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- bootstrap links -->
+    <!-- fonts links -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <!-- fonts links -->
+</head>
+<body>
+        <!-- login -->
+        <div class="container login">
+            <div class="row">
+                <div class="col-md-4" id="side1">
+                    <h3>Click Login to Buy Something</h3>
+                    <div id="btn"><a href="login.php">Login</a></div>
+                </div>
+                <div class="col-md-8" id="side2">
+                    <h3> Congratulations! Your Account Has Been Created successfully.</h3>
+                    <p><h3>Thank You For Creating Account.<h3></p>
+                </div>
+            </div>
+        </div>
+        <!-- login -->
+
+
+
+
+
+
+
+
+
+<!-- footer -->
+<footer id="footer" style="margin-top: 50px;">
+<div class="footer-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 footer-content">
+                <h3>E-Electronic</h3>
+                <p>E-Electronic, or electronic commerce (e-commerce), refers to the buying and selling of goods and services over the internet.</p>
+                <p>
+                    Bhaisepati <br>
+                    Lalitpur<br>
+                    Nepal<br>
+                </p>
+                <strong><i class="fas fa-phone"></i> Phone: <strong>+97798********</strong></strong><br>
+                <strong><i class="fa-solid fa-envelope"></i> Email: <strong>prabesh.timalsina1234@gmail.com</strong></strong>
+            </div>
+            <div class="col-lg-3 col-md-6 footer-links">
+                <ul>
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Our Services</h4>
+                <p>We are here to delivert products in time!!...</p>
+            </div>
+            <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Our Social World</h4>
+                <p>Our Social Worlds is for Providing services and to our valuable customer......</p>
+                <div class="socail-links mt-3">
+                    <a href="https://www.facebook.com/prabesh.timalsina.58" target="_blank" class="facebook"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/prash9356/" target="_blank" class="instagram"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/manabendra-timalsina-ab147a277/" target="_blank" class="linkin"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<div class="container py-4">
+    <div class="copyright">
+        &copy; Copyright <strong>E-Electronics</strong>.All Rights Reserved
+    </div>
+    <div class="credits">
+        Designed By <a href="#">Manabendra Timalsina</a>
+    </div>
+</div>
+</footer>
+<!-- footer -->
+<script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
