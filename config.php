e803fcc32f2e54532c9e90045d2d7a904b9a773d
timalsinamanabendra <97384274+timalsinamanabendra@users.noreply.github.com>
2023-08-09 01:48:28 +0545

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$dbhost ='localhost';
+$dbName = 'ecommerce';
+$daUsername = 'root';
+$dbPassword = '';
+
+$conn = mysqli_connect('localhost','root','','ecommerce');
+
+
+?>
